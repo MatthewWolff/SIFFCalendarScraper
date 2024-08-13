295c07c5c0897bb4c4eb2d9111f6ffedb5585e9c
Matthew Wolff <matthew.wolff@wisc.edu>
2024-08-13 14:18:36 -0700

    fix: new f-string wasn't python 3.11 compatible

--- model.py
+++ model.py
@@ -25,7 +25,7 @@ class HashableMovieEvent:
     showtime: ShowTime
 
     def __make_id_string(self):
-        return f"{self.title} {self.year} {self.showtime.start_time.isoformat(timespec="seconds")} {self.location}"
+        return f"{self.title} {self.year} {self.showtime.start_time.isoformat(timespec='seconds')} {self.location}"
 
     def __eq__(self, other):
         return other and self.__make_id_string() == other.__make_id_string()
