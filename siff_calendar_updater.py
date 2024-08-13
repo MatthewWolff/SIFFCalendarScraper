4c842f671150cc2b809aa0ac9f1b2dc2b925c072
MatthewWolff <mtwol@amazon.com>
2024-08-12 17:57:07 -0700

    refactored data model hashing to be cleaner, added country

--- siff_calendar_updater.py
+++ siff_calendar_updater.py
@@ -1,3 +1,4 @@
+import logging
 import os.path
 from datetime import datetime
 
@@ -6,8 +7,8 @@ from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 
+from model import MovieShowing, ShowTime, Theatre, HashableMovieEvent
 from siff_scraper import scrape_showings
-from model import MovieShowing, ShowTime, Theatre
 from util import parse_int, get_logger
 
 credentials_file = 'credentials.json'
@@ -21,42 +22,48 @@ logger = get_logger(__name__)
 
 
 def _get_credentials():
+    logging.debug("Retrieving credentials")
     credentials = None
     # The file token.json stores the user's access and refresh tokens, and is
     # created automatically when the authorization flow completes for the first
     # time.
     if os.path.exists(token_file):
+        logging.debug("Found existing token file")
         credentials = Credentials.from_authorized_user_file(token_file, SCOPES)
         # If there are no (valid) credentials available, let the user log in.
 
     if not (credentials and credentials.valid):
         if credentials and credentials.expired and credentials.refresh_token:
+            logging.debug("Refreshing expired token")
             credentials.refresh(Request())
         else:
+            logging.warning("New credentials required")
             flow = InstalledAppFlow.from_client_secrets_file(credentials_file, SCOPES)
             credentials = flow.run_local_server(port=0)
+
         # Save the credentials for the next run
         with open(token_file, "w") as token:
             token.write(credentials.to_json())
     return credentials
 
 
-def _extract_movie(calendar_event) -> MovieShowing:
+def _extract_movie(calendar_event) -> HashableMovieEvent:
+    """
+    Extract a MovieEvent from the calendar event.
+    Note: MovieEvents and MovieShowings share a hashing function
+    """
     title = calendar_event["summary"].lstrip("Movie: ")[:-len("(YEAR)")].strip()
     year = str(parse_int(calendar_event["summary"][-len("(YEAR)"):]))
     showtime = datetime.fromisoformat(calendar_event["start"]["dateTime"])
-    # create a minimal movie showing with only the attributes used in the hash function
-    return MovieShowing(title=title,
-                        year=year,
-                        showtime=ShowTime(showtime, showtime),
-                        director=str(), description=str(), location=str(), link=str(), duration_minutes=-1)
+    location = calendar_event["location"]
+    return HashableMovieEvent(title=title, year=year, showtime=ShowTime(showtime, None), location=location)
 
 
 def _create_event(movie: MovieShowing):
     return {
         "summary": f"Movie: {movie.title} ({movie.year})",
         "location": movie.location,
-        "description": f"Director: {movie.director}\n{movie.description}\n---\n{movie.link}",
+        "description": f"Director: {movie.director} - {movie.country}\n{movie.description}\n---\n{movie.link}",
         "start": {"dateTime": movie.showtime.start_time.isoformat(timespec="seconds"),
                   'timeZone': 'America/Los_Angeles'},
         "end": {"dateTime": movie.showtime.end_time.isoformat(timespec="seconds"),
@@ -68,7 +75,9 @@ def update_calendar(theatre: Theatre = Theatre.SIFF_CINEMA_EGYPTIAN):
     api_credentials = _get_credentials()
     service = build('calendar', 'v3', credentials=api_credentials)
 
+    logger.debug("Retrieving existing events from Google Calendar")
     events = service.events().list(calendarId=calendar_id).execute()
+    logger.debug(f"Found {len(events["items"])} existing events on calendar")
     current_showings = {_extract_movie(e) for e in events["items"]}
     for showing in scrape_showings(theatre):
         if showing not in current_showings:
