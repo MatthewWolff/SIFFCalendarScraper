c1e55cb52761cb442eae5131ea94d322089450de
MatthewWolff <mtwol@amazon.com>
2024-08-13 16:07:14 -0700

    fix: edge case where movie metadata was missing country

--- test/util_test.py
+++ test/util_test.py
@@ -1,8 +1,6 @@
-import logging
 import unittest
-from datetime import datetime, timedelta
 
-from util import get_logger, parse_int, get_date_delta, get_datetime_from_milliseconds, PACIFIC_TIMEZONE
+from util import *
 
 
 class TestUtilMethods(unittest.TestCase):
@@ -24,6 +22,19 @@ class TestUtilMethods(unittest.TestCase):
         milliseconds = 1723449313000
         expected_datetime = datetime(2024, 8, 12, 0, 55, 13, tzinfo=PACIFIC_TIMEZONE)
         self.assertEqual(get_datetime_from_milliseconds(str(milliseconds)).date(), expected_datetime.date())
+
+    def test_is_parseable_as_int(self):
+        # Test cases where the input should be parseable as an integer
+        self.assertTrue(is_parseable_as_int("123"))
+        self.assertTrue(is_parseable_as_int("-456"))
+        self.assertTrue(is_parseable_as_int("0"))
+        self.assertTrue(is_parseable_as_int("+789"))
+
+        # Test cases where the input should not be parseable as an integer
+        self.assertFalse(is_parseable_as_int("abc"))
+        self.assertFalse(is_parseable_as_int("3.14"))
+        self.assertFalse(is_parseable_as_int(""))
+        self.assertFalse(is_parseable_as_int("123.45"))
 
 
 if __name__ == '__main__':
