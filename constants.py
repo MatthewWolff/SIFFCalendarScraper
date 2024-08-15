1dbcc5b696890e036f44fea2873d815774072419
Matthew Wolff <matthew.wolff@wisc.edu>
2024-08-15 16:34:48 -0700

    added scraper tests

--- constants.py
+++ constants.py
@@ -1,5 +1,7 @@
 from enum import StrEnum
 
+import pytz
+
 
 # the string value must match the one used in the SIFF website URL
 class SIFFTheatre(StrEnum):
@@ -14,3 +16,7 @@ class GoogleCalendar(StrEnum):
     SIFF_CINEMA_DOWNTOWN = "a03eefa417a8c79ec4d4007ef921a99c601966da78b241fee2e1bfa6594f05b5@group.calendar.google.com"
     SIFF_CINEMA_UPTOWN = "b152bb62cb1456b2521f6beef6e1a169ca7a382f2e325cd114286ff434ea79db@group.calendar.google.com"
     SIFF_FILM_CENTER = "dbd809e897c3d7a8f2f13a63d3549d5141d4f848201ad1824daa61ec35769591@group.calendar.google.com"
+
+
+PACIFIC_TIMEZONE = pytz.timezone('America/Los_Angeles')
+MILLISEC_PER_SEC = 1000
--- util.py
+++ util.py
@@ -1,13 +1,12 @@
 import logging
+import os
 import re
 from datetime import datetime, timedelta
 
-import pytz
+from bs4 import BeautifulSoup
 
-from constants import GoogleCalendar
-
-MILLISEC_PER_SEC = 1000
-PACIFIC_TIMEZONE = pytz.timezone('America/Los_Angeles')
+from constants import GoogleCalendar, PACIFIC_TIMEZONE, MILLISEC_PER_SEC
+from model import ShowTime
 
 
 def get_logger(filename, level=logging.INFO):
@@ -48,3 +47,29 @@ def is_parseable_as_int(string) -> bool:
 
 def get_calendar_name(calendar: GoogleCalendar) -> str:
     return next(name for name, value in vars(GoogleCalendar).items() if value == calendar.value)
+
+
+def read_html_files(directory):
+    html_files = {}
+    print()
+
+    for root, dirs, files in os.walk(directory):
+        for file in files:
+            if file.endswith('.html'):
+                file_path = os.path.join(root, file)
+                relative_path = os.path.relpath(file_path, directory)
+                directory_path, _ = os.path.split(relative_path)
+
+                if directory_path not in html_files:
+                    html_files[directory_path] = {}
+
+                with open(file_path, 'r', encoding='utf-8') as f:
+                    html_content = BeautifulSoup(f.read(), 'html.parser')
+                    html_files[directory_path][file] = html_content
+
+    return html_files
+
+
+def assert_equal_showtime(t1: ShowTime, t2: ShowTime) -> bool:
+    return (t1.start_time.replace(tzinfo=None) == t2.start_time.replace(tzinfo=None) and
+            t1.end_time.replace(tzinfo=None) == t2.end_time.replace(tzinfo=None))
